a9b95b867db7f96d383173b831070e2978885693
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-08-01 07:39:23 -0700

    commit

--- channel_subscription_v2.py
+++ channel_subscription_v2.py
@@ -4,13 +4,11 @@
 import yaml
 import time
 from telegram.ext import Updater, MessageHandler, Filters
-import requests
 import threading
 import export_to_telegraph
-from bs4 import BeautifulSoup
-import hashlib
 from telegram_util import splitCommand, log_on_fail, autoDestroyss
 import plain_db
+import webgram
 
 with open('credential') as f:
     credential = yaml.load(f, Loader=yaml.FullLoader)
@@ -22,10 +20,16 @@ existing = plain_db.loadKeyOnlyDB('existing')
 with open('db') as f:
     db = yaml.load(f, Loader=yaml.FullLoader)
 def saveDB():
+    for chat_id in list(db.keys()):
+        if not isinstance(chat_id, int):
+            continue
+        try:
+            r = tele.bot.send_message(chat_id, 'test')
+            r.delete()
+        except:
+            del db[chat_id]
     with open('db', 'w') as f:
         f.write(yaml.dump(db, sort_keys=True, indent=2, allow_unicode=True))
-
-INTERVAL = 3600
 
 def listPool(msg):
     items = ['%d: [%s](t.me/%s)' % (index, content, content)
@@ -65,22 +69,7 @@ def start(update, context):
     if update.message:
         update.message.reply_text(help_message)
 
-def getSoup(url):
-    headers = {'Host':'telete.in',
-        'Connection':'keep-alive',
-        'Cache-Control':'max-age=0',
-        'Upgrade-Insecure-Requests':'1',
-        'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36',
-        'Sec-Fetch-User':'?1',
-        'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
-        'Sec-Fetch-Site':'none',
-        'Sec-Fetch-Mode':'navigate',
-        'Accept-Encoding':'gzip, deflate, br',
-        'Accept-Language':'en-US,en;q=0.9,zh;q=0.8,zh-CN;q=0.7'}
-    r = requests.get(url, headers=headers)
-    return BeautifulSoup(r.text, 'html.parser')
-
-def getParsedText(text):
+def getMessage(text, toTelegraph=True):
     result = ''
     for item in text:
         if item.name in set(['br']):
@@ -90,53 +79,41 @@ def getParsedText(text):
             if item.text:
                 result += '<i>' + item.text + '</i>'
             continue
-        if item.name == 'a':
-            telegraph_url = export_to_telegraph.export(item['href'])
-            if telegraph_url:
-                item['href'] = telegraph_url
-                del item['rel']
-                if 'http' in item.text:
-                    item.contents[0].replaceWith(telegraph_url)
-        if str(item).startswith('原文') and 'telegra' in result:
+        if 'telegra' in result and (str(item).startswith('原文') or 
+            str(item.text).startswith('原文')):
             return result
+        if item.name == 'a':
+            if 'rel' in item:
+                del item['rel']
+            if toTelegraph:
+                telegraph_url = export_to_telegraph.export(item['href'])
+                if telegraph_url:
+                    item['href'] = telegraph_url
+                    if 'http' in item.text:
+                        item.contents[0].replaceWith(telegraph_url)
         result += str(item)
     return result
 
-def keyMatch(chat_id, author, result):
-    if (not isinstance(chat_id, int)) or (not db[chat_id]):
-        return False
-    for key in db[chat_id]:
-        if key in str(author) or key in str(result):
-            return True
-    return False
-
-def intersect(l1, l2):
-    return set(l1).intersection(l2)
+def getMatches(index):
+    for chat_id in db:
+        if not isinstance(chat_id, int):
+            continue
+        if matchKey(index, db[chat_id]):
+            yield chat_id
 
 @log_on_fail(debug_group)
 def loopImp():
-    for item in db['pool']:
-        post = webgram.getPosts('')
-        for msg in soup.find_all('div', class_='tgme_widget_message_bubble'):
-            text = msg.find('div', class_='tgme_widget_message_text')
-            if (not text) or (not text.text):
+    for channel in db['pool']:
+        for post in webgram.getPosts(channel)[1:]:
+            maintext = post.getMaintext()
+            if not existing.add(maintext):
                 continue
-            hash_value = hashlib.sha224(str(text.text).encode('utf-8')).hexdigest()
-            if hash_value in hashes:
+            matches = list(getMatches(post.getIndex()))
+            if not matches:
                 continue
-            author = msg.find('div', class_='tgme_widget_message_author')
-            result = getParsedText(text)
-            matches = [chat_id for chat_id in db if keyMatch(chat_id, str(author), result)]
+            message = getMessage(post.text, channel != 'twitter_subscriptions')
             for chat_id in matches:
-                try:
-                    tele.bot.send_message(chat_id=chat_id, text=result, parse_mode='HTML')
-                    time.sleep(1)
-                except Exception as e:
-                    print(chat_id)
-                    print(e)                        
-                    print(result)
-            hashes.add(hash_value)
-            saveHashes(hash_value)
+                tele.bot.send_message(chat_id, message, parse_mode='HTML')
 
 def loop():
     loopImp()
